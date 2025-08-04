e86e974e0b1f8396ae603e85b91297bdf8a92b79
Tamradhwaj Ojha <tamradhwajojhaji@gmail.com>
2025-08-04 23:38:50 +0530

    introduction to SQL

---
+++ 11 mysql/util/database-util.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'node-complete',
+  password: 'Tamradhwaj@3', // replace with your actual password
+  database: 'airbnb',
+}); 
+
+module.exports = pool.promise(); // Exporting the promise-based pool for use in other modules
--- 9 MVC/models/Home.js
+++ 9 MVC/models/Home.js
@@ -1,67 +1,33 @@
 const fs = require('fs');
 const path = require('path');
-const rootDir = require('../util/path-util'); // Importing the root directory path utility
+const rootDir = require('./../util/path-util');
 
-const homefilePath = path.join(rootDir, 'data', 'homes.json'); // Define the path to the homes data file
-
-// Ensure data directory exists
-const dataDir = path.dirname(homefilePath);
-if (!fs.existsSync(dataDir)) {
-    fs.mkdirSync(dataDir, { recursive: true });
-}
-
-// Initialize homes.json if it doesn't exist
-if (!fs.existsSync(homefilePath)) {
-    fs.writeFileSync(homefilePath, JSON.stringify([]));
-}
+const homeFilePath = path.join(rootDir, 'data', 'homes.json');
 
 module.exports = class Home {
-   constructor(houseName, price, location, rating, photo){
+  constructor(houseName, price, location, rating, photo) {
     this.houseName = houseName;
     this.price = price;
     this.location = location;
     this.rating = rating;
     this.photo = photo;
-   }
+  }
+sssssssssss
+  save(callback) {
+    Home.fetchAll(registeredHomes => {
+      registeredHomes.push(this);
 
-   save(callback){
-    // Read existing homes first
-    let homes = [];
-    try {
-        const fileContent = fs.readFileSync(homefilePath);
-        homes = JSON.parse(fileContent);
-    } catch (error) {
-        console.error('Error reading homes file:', error);
-        homes = [];
-    }
-    
-    // Add new home
-    homes.push(this);
-    
-    // Write back to file
-    fs.writeFile(homefilePath, JSON.stringify(homes), (error) => {
-      if (error) {
-        console.error('Error writing to homes file:', error);
-      } else {
-        console.log('Home saved successfully');
-      }
+      fs.writeFile(homeFilePath, JSON.stringify(registeredHomes), callback);
     });
-   }
-   
+  }
+
   static fetchAll(callback) {
-    fs.readFile(homefilePath, (error, fileContent) => {
-      if (error) {  
-        console.error('Error reading homes file:', error);
-        callback([]); // Return an empty array if there's an error
+    fs.readFile(homeFilePath, (err, data) => {
+      if (err) {
+        callback([]);
       } else {
-        try {
-          const homes = JSON.parse(fileContent);
-          callback(homes); // Return the parsed homes via the callback
-        } catch (parseError) {
-          console.error('Error parsing homes file:', parseError);
-          callback([]);
-        }
+        callback(JSON.parse(data));
       }
-    });
+    })
   }
 }
