2a890b09454d7715adeda6f9226fe100bbc87994
Tamradhwaj Ojha <139583879+tamradhwajojha@users.noreply.github.com>
2025-09-02 15:42:58 +0530

    Update app.js

--- 8 Dynamic ui/app.js
+++ 8 Dynamic ui/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 
 const bodyParser = require('body-parser');
-//external module
+//external modules
 const express = require('express'); 
 
 const rootDir = require('./util/path-util'); // Assuming path-util.js is in the util directory
